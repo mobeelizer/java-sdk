1159018fdafd9e9bbd796256989476940e655e25
Maciej Szczytowski <maciej.szczytowski@mobeelizer.com>
2012-07-25 10:40:32 +0200

    store owner's group in entities

--- src/main/java/com/mobeelizer/java/MobeelizerSyncService.java
+++ src/main/java/com/mobeelizer/java/MobeelizerSyncService.java
@@ -28,215 +28,218 @@ import com.mobeelizer.java.sync.MobeelizerOutputData;
 
 public class MobeelizerSyncService {
 
-    private static final Logger logger = LoggerFactory.getLogger(MobeelizerSyncService.class);
+	private static final Logger logger = LoggerFactory.getLogger(MobeelizerSyncService.class);
 
-    private final MobeelizerConnectionService connectionService;
+	private final MobeelizerConnectionService connectionService;
 
-    private final Map<Class<?>, MobeelizerModelImpl> definitionByClass = new HashMap<Class<?>, MobeelizerModelImpl>();
+	private final Map<Class<?>, MobeelizerModelImpl> definitionByClass = new HashMap<Class<?>, MobeelizerModelImpl>();
 
-    private final Map<String, MobeelizerModelImpl> definitionByName = new HashMap<String, MobeelizerModelImpl>();
+	private final Map<String, MobeelizerModelImpl> definitionByName = new HashMap<String, MobeelizerModelImpl>();
 
-    private boolean hasDefinition = false;
+	private boolean hasDefinition = false;
 
-    public MobeelizerSyncService(final Set<MobeelizerModel> definition, final MobeelizerConnectionService connectionService) {
-        if (definition != null) {
-            hasDefinition = true;
-            for (MobeelizerModel model : definition) {
-                definitionByClass.put(model.getMappingClass(), (MobeelizerModelImpl) model);
-                definitionByName.put(model.getName(), (MobeelizerModelImpl) model);
-            }
-        }
-        this.connectionService = connectionService;
-    }
+	public MobeelizerSyncService(final Set<MobeelizerModel> definition, final MobeelizerConnectionService connectionService) {
+		if (definition != null) {
+			hasDefinition = true;
+			for (MobeelizerModel model : definition) {
+				definitionByClass.put(model.getMappingClass(), (MobeelizerModelImpl) model);
+				definitionByName.put(model.getName(), (MobeelizerModelImpl) model);
+			}
+		}
+		this.connectionService = connectionService;
+	}
 
-    public void syncAll(final MobeelizerSyncCallback callback) {
-        sync(null, null, callback, true);
-    }
+	public void syncAll(final MobeelizerSyncCallback callback) {
+		sync(null, null, callback, true);
+	}
 
-    public void sync(final Iterable<Object> entities, final Iterable<MobeelizerFile> files, final MobeelizerSyncCallback callback) {
-        sync(entities, files, callback, false);
-    }
+	public void sync(final Iterable<Object> entities, final Iterable<MobeelizerFile> files, final MobeelizerSyncCallback callback) {
+		sync(entities, files, callback, false);
+	}
 
-    private void sync(final Iterable<Object> outputEntities, final Iterable<MobeelizerFile> outputFiles,
-            final MobeelizerSyncCallback callback, final boolean isAllSynchronization) {
-        final MobeelizerInputData inputData;
+	private void sync(final Iterable<Object> outputEntities, final Iterable<MobeelizerFile> outputFiles,
+			final MobeelizerSyncCallback callback, final boolean isAllSynchronization) {
+		final MobeelizerInputData inputData;
 
-        File outputFile = null;
-        File inputFile = null;
+		File outputFile = null;
+		File inputFile = null;
 
-        try {
-            final String ticket;
+		try {
+			final String ticket;
 
-            if (isAllSynchronization) {
-                ticket = connectionService.sendSyncAllRequest();
-            } else {
-                outputFile = File.createTempFile("mobeelizer", "sync");
+			if (isAllSynchronization) {
+				ticket = connectionService.sendSyncAllRequest();
+			} else {
+				outputFile = File.createTempFile("mobeelizer", "sync");
 
-                MobeelizerErrorsHolder errors = new MobeelizerErrorsHolder();
+				MobeelizerErrorsHolder errors = new MobeelizerErrorsHolder();
 
-                prepareOutputFile(outputFile, outputEntities, outputFiles, errors);
+				prepareOutputFile(outputFile, outputEntities, outputFiles, errors);
 
-                if (!errors.isValid()) {
-                    callback.onSyncFinishedWithError(errors);
-                    return;
-                }
+				if (!errors.isValid()) {
+					callback.onSyncFinishedWithError(errors);
+					return;
+				}
 
-                ticket = connectionService.sendSyncDiffRequest(outputFile);
-            }
+				ticket = connectionService.sendSyncDiffRequest(outputFile);
+			}
 
-            MobeelizerConnectionResult result = connectionService.waitUntilSyncRequestComplete(ticket);
-            if (!result.isSuccess()) {
-                callback.onSyncFinishedWithError(new IllegalStateException(result.getMessage()));
-                return;
-            }
+			MobeelizerConnectionResult result = connectionService.waitUntilSyncRequestComplete(ticket);
+			if (!result.isSuccess()) {
+				callback.onSyncFinishedWithError(new IllegalStateException(result.getMessage()));
+				return;
+			}
 
-            inputFile = connectionService.getSyncData(ticket);
+			inputFile = connectionService.getSyncData(ticket);
 
-            inputData = new MobeelizerInputData(new FileInputStream(inputFile), File.createTempFile("mobeelizer", "input"));
+			inputData = new MobeelizerInputData(new FileInputStream(inputFile), File.createTempFile("mobeelizer", "input"));
 
-            callback.onSyncFinishedWithSuccess(prepareInputEntitiesIterator(inputData), prepareInputFileIterator(inputData),
-                    inputData.getDeletedFiles(), new MobeelizerConfirmSyncCallback() {
+			callback.onSyncFinishedWithSuccess(prepareInputEntitiesIterator(inputData), prepareInputFileIterator(inputData),
+					inputData.getDeletedFiles(), new MobeelizerConfirmSyncCallback() {
 
-                        @Override
-                        public void confirm() {
-                            try {
-                                connectionService.confirmTask(ticket);
-                            } catch (IOException e) {
-                                logger.warn("Cannot confirm task: " + e.getMessage(), e);
-                            } finally {
-                                if (inputData != null) {
-                                    inputData.close();
-                                }
-                            }
-                        }
+						@Override
+						public void confirm() {
+							try {
+								connectionService.confirmTask(ticket);
+							} catch (IOException e) {
+								logger.warn("Cannot confirm task: " + e.getMessage(), e);
+							} finally {
+								if (inputData != null) {
+									inputData.close();
+								}
+							}
+						}
 
-                    });
-        } catch (Exception e) {
-            callback.onSyncFinishedWithError(e);
-        } finally {
-            if (outputFile != null && !outputFile.delete()) {
-                logger.warn("Cannot delete file " + outputFile.getAbsolutePath());
-            }
-            if (inputFile != null && !inputFile.delete()) {
-                logger.warn("Cannot delete file " + inputFile.getAbsolutePath());
-            }
-        }
-    }
+					});
+		} catch (Exception e) {
+			callback.onSyncFinishedWithError(e);
+		} finally {
+			if (outputFile != null && !outputFile.delete()) {
+				logger.warn("Cannot delete file " + outputFile.getAbsolutePath());
+			}
+			if (inputFile != null && !inputFile.delete()) {
+				logger.warn("Cannot delete file " + inputFile.getAbsolutePath());
+			}
+		}
+	}
 
-    private Iterable<MobeelizerFile> prepareInputFileIterator(final MobeelizerInputData inputData) throws IOException {
-        List<MobeelizerFile> files = new ArrayList<MobeelizerFile>();
+	private Iterable<MobeelizerFile> prepareInputFileIterator(final MobeelizerInputData inputData) throws IOException {
+		List<MobeelizerFile> files = new ArrayList<MobeelizerFile>();
 
-        for (final String file : inputData.getFiles()) {
-        	InputStream input = inputData.getFile(file);
-        	ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int len;
-            while ((len = input.read(buffer)) > -1 ) {
-                baos.write(buffer, 0, len);
-            }
-            baos.flush();
-            final InputStream copyInput = new ByteArrayInputStream(baos.toByteArray());
-        	
-            files.add(new MobeelizerFile() {
+		for (final String file : inputData.getFiles()) {
+			InputStream input = inputData.getFile(file);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			byte[] buffer = new byte[1024];
+			int len;
+			while ((len = input.read(buffer)) > -1) {
+				baos.write(buffer, 0, len);
+			}
+			baos.flush();
+			final InputStream copyInput = new ByteArrayInputStream(baos.toByteArray());
 
-                @Override
-                public String getName() {
-                    return null;
-                }
+			files.add(new MobeelizerFile() {
 
-                @Override
-                public InputStream getInputStream() {
-                	return copyInput;
-                }
+				@Override
+				public String getName() {
+					return null;
+				}
 
-                @Override
-                public String getGuid() {
-                    return file;
-                }
+				@Override
+				public InputStream getInputStream() {
+					return copyInput;
+				}
 
-                @Override
-                public File getFile() {
-                    throw new UnsupportedOperationException();
-                }
+				@Override
+				public String getGuid() {
+					return file;
+				}
 
-            });
-        }
+				@Override
+				public File getFile() {
+					throw new UnsupportedOperationException();
+				}
 
-        return files;
-    }
+			});
+		}
 
-    private MobeelizerModelImpl getModel(final Class<?> clazz) {
-        if (!definitionByClass.containsKey(clazz)) {
-            throw new IllegalStateException("Class " + clazz.getCanonicalName() + " not mapped.");
-        }
-        return definitionByClass.get(clazz);
-    }
+		return files;
+	}
 
-    private MobeelizerModelImpl getModel(final String name) {
-        if (!definitionByName.containsKey(name)) {
-            throw new IllegalStateException("Class for model " + name + " not mapped.");
-        }
-        return definitionByName.get(name);
-    }
+	private MobeelizerModelImpl getModel(final Class<?> clazz) {
+		if (!definitionByClass.containsKey(clazz)) {
+			throw new IllegalStateException("Class " + clazz.getCanonicalName() + " not mapped.");
+		}
+		return definitionByClass.get(clazz);
+	}
 
-    private Iterable<Object> prepareInputEntitiesIterator(final MobeelizerInputData inputData) {
+	private MobeelizerModelImpl getModel(final String name) {
+		if (!definitionByName.containsKey(name)) {
+			throw new IllegalStateException("Class for model " + name + " not mapped.");
+		}
+		return definitionByName.get(name);
+	}
 
-        List<Object> entities = new ArrayList<Object>();
+	private Iterable<Object> prepareInputEntitiesIterator(final MobeelizerInputData inputData) {
 
-        for (MobeelizerJsonEntity entity : inputData.getInputData()) {
-            if (hasDefinition) {
-                entities.add(getModel(entity.getModel()).getEntityFromJsonEntity(entity));
-            } else {
-                Map<String, String> entityMap = new HashMap<String, String>(entity.getFields());
-                entityMap.put("model", entity.getModel());
-                entityMap.put("guid", entity.getGuid());
-                entityMap.put("owner", entity.getOwner());
-                entityMap.put("conflicted", (entity.getConflictState() != ConflictState.NO_IN_CONFLICT) + "");
-                entities.add(entityMap);
-            }
-        }
-        return entities;
-    }
+		List<Object> entities = new ArrayList<Object>();
 
-    private void prepareOutputFile(final File outputFile, final Iterable<Object> entities, final Iterable<MobeelizerFile> files,
-            final MobeelizerErrorsHolder errors) {
-        MobeelizerOutputData outputData = null;
+		for (MobeelizerJsonEntity entity : inputData.getInputData()) {
+			if (hasDefinition) {
+				entities.add(getModel(entity.getModel()).getEntityFromJsonEntity(entity));
+			} else {
+				Map<String, String> entityMap = new HashMap<String, String>(entity.getFields());
+				entityMap.put("model", entity.getModel());
+				entityMap.put("guid", entity.getGuid());
+				entityMap.put("owner", entity.getOwner());
+				entityMap.put("group", entity.getGroup());
+				entityMap.put("conflicted", (entity.getConflictState() != ConflictState.NO_IN_CONFLICT) + "");
+				entities.add(entityMap);
+			}
+		}
+		return entities;
+	}
 
-        try {
-            outputData = new MobeelizerOutputData(outputFile, File.createTempFile("mobeelizer", "output"));
+	private void prepareOutputFile(final File outputFile, final Iterable<Object> entities, final Iterable<MobeelizerFile> files,
+			final MobeelizerErrorsHolder errors) {
+		MobeelizerOutputData outputData = null;
 
-            if (entities != null) {
-                for (Object entity : entities) {
-                    if (hasDefinition) {
-                        outputData.writeEntity(getModel(entity.getClass()).getJsonEntityFromEntity(entity, errors));
-                    } else {
-                        @SuppressWarnings("unchecked")
-                        Map<String, String> entityMap = new HashMap<String, String>((Map<String, String>) entity);
-                        MobeelizerJsonEntity jsonEntity = new MobeelizerJsonEntity();
-                        jsonEntity.setModel(entityMap.get("model"));
-                        jsonEntity.setGuid(entityMap.get("guid"));
-                        jsonEntity.setOwner(entityMap.get("owner"));
-                        entityMap.remove("model");
-                        entityMap.remove("guid");
-                        entityMap.remove("owner");
-                        jsonEntity.setFields(entityMap);
-                        outputData.writeEntity(jsonEntity);
-                    }
-                }
-            }
+		try {
+			outputData = new MobeelizerOutputData(outputFile, File.createTempFile("mobeelizer", "output"));
 
-            if (files != null) {
-                for (MobeelizerFile file : files) {
-                    outputData.writeFile(file.getGuid(), file.getInputStream());
-                }
-            }
+			if (entities != null) {
+				for (Object entity : entities) {
+					if (hasDefinition) {
+						outputData.writeEntity(getModel(entity.getClass()).getJsonEntityFromEntity(entity, errors));
+					} else {
+						@SuppressWarnings("unchecked")
+						Map<String, String> entityMap = new HashMap<String, String>((Map<String, String>) entity);
+						MobeelizerJsonEntity jsonEntity = new MobeelizerJsonEntity();
+						jsonEntity.setModel(entityMap.get("model"));
+						jsonEntity.setGuid(entityMap.get("guid"));
+						jsonEntity.setOwner(entityMap.get("owner"));
+						jsonEntity.setGroup(entityMap.get("group"));
+						entityMap.remove("model");
+						entityMap.remove("guid");
+						entityMap.remove("owner");
+						entityMap.remove("group");
+						jsonEntity.setFields(entityMap);
+						outputData.writeEntity(jsonEntity);
+					}
+				}
+			}
 
-        } catch (IOException e) {
-            throw new IllegalStateException(e.getMessage(), e);
-        } finally {
-            if (outputData != null) {
-                outputData.close();
-            }
-        }
-    }
+			if (files != null) {
+				for (MobeelizerFile file : files) {
+					outputData.writeFile(file.getGuid(), file.getInputStream());
+				}
+			}
+
+		} catch (IOException e) {
+			throw new IllegalStateException(e.getMessage(), e);
+		} finally {
+			if (outputData != null) {
+				outputData.close();
+			}
+		}
+	}
 }
